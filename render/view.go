4e00a571c697d102a9b45e7a1046752114f7cc83
abused_master <techperson71@gmail.com>
2020-03-27 19:53:33 -0400

    Added transform and view matrix funcs

---
+++ render/view.go
@@ -0,0 +1,22 @@
+package render
+
+import "github.com/go-gl/mathgl/mgl64"
+
+func createTransformMatrix(translation mgl64.Vec3, rotation mgl64.Quat, scale float64) mgl64.Mat4 {
+	matrix := mgl64.Ident4()
+	mgl64.TransformCoordinate(translation, matrix)
+	matrix.Mul4(mgl64.HomogRotate3DX(rotation.X()))
+	matrix.Mul4(mgl64.HomogRotate3DY(rotation.Y()))
+	matrix.Mul4(mgl64.HomogRotate3DZ(rotation.Z()))
+	matrix.Mul4(mgl64.Scale3D(scale, scale, scale))
+
+	return matrix
+}
+
+func createViewMatrix(position mgl64.Vec3, rotation mgl64.Quat) mgl64.Mat4 {
+	matrix := mgl64.LookAtV(position.Mul(-1), mgl64.Vec3{0, 0, 0}, mgl64.Vec3{0, 1, 0})
+	matrix.Mul4(mgl64.HomogRotate3DX(rotation.X()))
+	matrix.Mul4(mgl64.HomogRotate3DY(rotation.Y()))
+
+	return matrix
+}
